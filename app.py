e29f0f53aa9405fa4c17dbf41142bfabeca9052a
kelechi oleka <66587705+khalkcee@users.noreply.github.com>
2023-05-21 02:03:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 from chat import get_response
-CORS(app, resources={r"/*": {"origins": "*"}})
 
- Access-Control-Allow-Origin: *
 app = Flask(__name__)
 CORS(app)
 
@@ -15,10 +13,10 @@ def index():
 @app.route("/predict", methods=["POST"])
 def predict():
     text = request.get_json().get("message")
-    # TODO: check if text is valid
-    response = get_response(text)
-    message = {"answer": response}
-    return jsonify(message)
+  
+    response = jsonify(message)
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
 
 if __name__ == "__main__":
     app.run(debug=True)
