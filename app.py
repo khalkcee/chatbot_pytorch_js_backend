9f28c42b317de56b71f628558e4ea624abf2eb4f
kelechi oleka <66587705+khalkcee@users.noreply.github.com>
2023-05-21 01:50:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 from chat import get_response
-
-app = Flask(__name__)
-CORS(app)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
  Access-Control-Allow-Origin: *
+app = Flask(__name__)
+CORS(app)
+
 
 @app.route("/")
 def index():
